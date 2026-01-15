9197cd1d6abd73f3b341abb97e76c72f7a1cf4c3
Soos3D <99700157+soos3d@users.noreply.github.com>
2026-01-15 15:18:37 -0300

    edit config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turbo: undefined,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      canvas: false,
+      encoding: false,
+    };
+    return config;
   },
-}
+};
 
 export default nextConfig;
